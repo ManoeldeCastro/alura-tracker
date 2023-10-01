07d331843e010278ca12c3b69e20572119b1448c
manoel <manoeldiasmf@gmail.com>
2023-10-01 18:10:05 -0300

    create task list and box default

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral/>
+    </div>
+    <div class="column is-three-quarter">
+      <OFormulario @aoSalvarTarefa="salvarTarefa"/>
+      <div class="lista">
+      <ATarefa v-for="(tarefa, i) in tarefas" :key="i"  :tarefa="tarefa"/>
+    </div>
+    <OBox v-if="tarefas.length === 0">
+      Você não esta muito produtivo hoje 😴
+    </OBox>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import BarraLateral from './components/BarraLateral.vue';
+import OFormulario from './components/OFormulario.vue';
+import ATarefa from './components/ATarefa.vue';
+import ITarefa from './interfaces/ITarefa';
+import OBox from './components/OBox.vue';
+
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
+    name: 'App',
+    components: {
+    BarraLateral,
+    OFormulario,
+    ATarefa,
+    OBox
+},
+  data(){
+    return {
+      tarefas: [] as ITarefa[]
   }
+},
+methods: {
+  salvarTarefa(tarefa: ITarefa){
+    this.tarefas.push(tarefa)
+  }
+}
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .lista{
+    padding: 1.25rem;
+  }
+
 </style>
